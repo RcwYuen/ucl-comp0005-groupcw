68bdee02f3a7cdde7878d4b85b8cd45b2d797060
RcwYuen <44876555+RcwYuen@users.noreply.github.com>
2021-02-09 18:07:07 +0800

    Add files via upload

---
+++ jarvismarch.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Jarvis march algorithm
+
+# Use the cell below for all python code needed to realise the Jarvis march algorithm (including any auxiliary data structures and functions you might need). The `jarvismarch()` function itself should take as input parameter a list of 2D points (`inputSet`), and return the subset of such points (`outputSet`) that lie on the convex hull.
+
+# In[8]:
+
+
+#def jarvismarch(inputSet):
+
+
+
+#    return outputSet
+
+
+# Use the cell below for all python code needed to generate test data points (both random and those representing worst-case scenario).
+
+# In[9]:
+
+
+import random
+
+#code for random data generation
+
+
+#code for worst case data generation
+
+
+# Use the cell below for all python code needed to test the `jarvismarch()` function on the data generated above.
+
+# In[10]:
+
+
+import timeit
+
+#test code
+
+
+# *Optional*: Feel free to use the code below on small datasets (e.g., N = 10) to visually inspect whether the algorithm has been implemented correctly. The fragment below assumes both `inputSet` and `outputSet` to be lists of 2D points, with each point being a list of 2 elements (e.g., `[[x1,y1], [x2,y2], ..., [x_k,y_k]]`)
+
+# In[14]:
+
+
+import matplotlib.pyplot as plt
+
+# inputSet and outputSet should have been defined above. 
+# uncomment the next two lines only if you wish to test the plotting code before coding your algorithm
+
+#inputSet = [[1,1], [2,2] , [3, 3], [4,4], [1,4], [3,1], [1, 5], [2, 4], [3, 5]]
+#outputSet = [[1,1], [3,1] , [4, 4], [3,5], [1,5]]
+
+plt.figure()
+
+#first do a scatter plot of the inputSet
+input_xs, input_ys = zip(*inputSet)
+plt.scatter(input_xs, input_ys)
+
+#then do a polygon plot of the computed covex hull
+outputSet.append(outputSet[0]) #first create a 'closed loop' by adding the first point at the end of the list
+output_xs, output_ys = zip(*outputSet) 
+plt.plot(output_xs, output_ys) 
+
+plt.show() 
+
+
+# In[ ]:
+
+
+
+
